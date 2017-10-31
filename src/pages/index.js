14ada2b7c0e651ca0f24a482fdb282320c4eb2da
Alice Young <aliceyoung.9@gmail.com>
2017-10-31 00:16:32 -0400

    Write function

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,16 @@ const Footer = () => (
   <a href='//aliceyoung.xyz'>aliceyoung.xyz</a>
 )
 
+const _calcDate = () => {
+  const lastAccident = new Date(2017, 9, 27, 15);
+  const today = new Date();
+  
+  return Math.floor( (today - lastAccident) / (1000 * 60 * 60 *24));
+}
+
 const IndexPage = () => (
   <div>
-    <h1>3 days since my last bike accident 🙃</h1>
+    <h1>{_calcDate()} days since my last bike accident 🙃</h1>
     <Footer/>
   </div>
 )
