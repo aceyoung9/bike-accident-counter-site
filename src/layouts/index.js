1b8a04a959a28eb7ecb8588e3a3c7fa9ac2edf6e
Alice Young <aliceyoung.9@gmail.com>
2017-10-31 01:30:38 -0400

    Style tweaking: type, colors.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ import '../styles/main.scss'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="lol how many days has it been since Alice got into a bike accident?"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Footer = () => (
-  <a href='//aliceyoung.xyz'>aliceyoung.xyz</a>
-)
-
 const _calcDate = () => {
   const lastAccident = new Date(2017, 9, 27, 15);
   const today = new Date();
-  
+
   return Math.floor( (today - lastAccident) / (1000 * 60 * 60 *24));
 }
 
+const Footer = () => (
+  <footer>
+    <a href='//aliceyoung.xyz'>aliceyoung.xyz</a>
+  </footer>
+)
+
 const IndexPage = () => (
   <div>
-    <h1>{_calcDate()} days since my last bike accident 🙃</h1>
+    <h1>
+      It has been <span className='highlight'>{_calcDate()} days</span> since my last bike accident 🙃
+    </h1>
     <Footer/>
   </div>
 )
