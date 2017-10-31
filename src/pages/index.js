a6244d10f839a4dedaf002cd8f7cdfdb23a93aaf
Alice Young <aliceyoung.9@gmail.com>
2017-10-31 17:32:13 -0400

    flesh out footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,27 @@ const _calcDate = () => {
 }
 
 const Footer = () => (
-  <footer>
-    <a href='//aliceyoung.xyz'>aliceyoung.xyz</a>
+  <footer className='mw9 center'>
+    <div className='cf'>
+      <div className='fl w-100 w-third-ns pa3'>
+        <h2>Made with pettiness by <a href='//aliceyoung.xyz'>aliceyoung.xyz</a></h2>
+      </div>
+      <div className='fl w-100 w-third-ns pa3'>
+        <h2>Boston Bike Advocacy Organizations I like</h2>
+        <ul>
+          <li><a href='http://bostoncyclistsunion.org/'>Boston Cyclists Union</a></li>
+          <li><a href='http://www.streetspacmass.org/'>StreetsPAC Mass</a></li>
+        </ul>
+      </div>
+      <div className='fl w-100 w-third-ns pa3'>
+        <h2>Send Thoughts and Prayers™</h2>
+        <ul>
+          <li>Indulge my <a href='https://venmo.com/alice9'>Venmo</a> lol</li>
+          <li>Twitter: <a href='//twitter.com/aliceyoung_9'>@aliceyoung_9</a></li>
+        </ul>
+      </div>
+    </div>
+
   </footer>
 )
 
