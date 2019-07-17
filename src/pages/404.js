cb8d1e98c19a0f1cddd9543a6f593c9fc0f2a57e
Ace Young <ace.young289@gmail.com>
2019-07-17 15:38:24 -0400

    💅 Run and upgrade Prettier.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const NotFoundPage = () => (
   <div>
     <h1>404</h1>
     <p>Seriously, it's a 1-page site.</p>
   </div>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
