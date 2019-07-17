a9e750b2fe3b79d63cb2aec8c83bcea5c2fb75d5
Ace Young <aliceyoung.9@gmail.com>
2019-07-17 15:11:51 -0400

    📦 Upgrade Gatsby to v2, add peer dependencies, compatibility fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,62 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Layout from "../components/layout";
 
 const _calcDate = () => {
   const lastAccident = new Date(2018, 3, 24, 13);
   const today = new Date();
 
-  return Math.floor( (today - lastAccident) / (1000 * 60 * 60 *24));
-}
+  return Math.floor((today - lastAccident) / (1000 * 60 * 60 * 24));
+};
 
 const Footer = () => (
-  <footer className='mw9 center'>
-    <div className='cf'>
-      <div className='fl w-100 w-third-ns pa3'>
-        <h2>Made with pettiness by <a href='//aliceyoung.xyz'>aliceyoung.xyz</a></h2>
-        <p>(Do you want the <a href='https://github.com/aliceyoung9/bike-accident-counter-site'>source code</a> or something?)</p>
+  <footer className="mw9 center">
+    <div className="cf">
+      <div className="fl w-100 w-third-ns pa3">
+        <h2>
+          Made with pettiness by <a href="//aliceyoung.xyz">aliceyoung.xyz</a>
+        </h2>
+        <p>
+          (Do you want the{" "}
+          <a href="https://github.com/aliceyoung9/bike-accident-counter-site">
+            source code
+          </a>{" "}
+          or something?)
+        </p>
       </div>
-      <div className='fl w-100 w-third-ns pa3'>
+      <div className="fl w-100 w-third-ns pa3">
         <h2>Boston Bike Advocacy Organizations I like</h2>
         <ul>
-          <li><a href='http://bostoncyclistsunion.org/'>Boston Cyclists Union</a></li>
-          <li><a href='http://www.streetspacmass.org/'>StreetsPAC Mass</a></li>
+          <li>
+            <a href="http://bostoncyclistsunion.org/">Boston Cyclists Union</a>
+          </li>
+          <li>
+            <a href="http://www.streetspacmass.org/">StreetsPAC Mass</a>
+          </li>
         </ul>
       </div>
-      <div className='fl w-100 w-third-ns pa3'>
+      <div className="fl w-100 w-third-ns pa3">
         <h2>Send Thoughts and Prayers™</h2>
         <ul>
-          <li>Indulge my <a href='https://venmo.com/alice9'>Venmo</a> lol</li>
-          <li>Twitter: <a href='//twitter.com/alice_young9'>@alice_young9</a></li>
+          <li>
+            Indulge my <a href="https://venmo.com/alice9">Venmo</a> lol
+          </li>
+          <li>
+            Twitter: <a href="//twitter.com/alice_young9">@alice_young9</a>
+          </li>
         </ul>
       </div>
     </div>
   </footer>
-)
+);
 
 const IndexPage = () => (
-  <div>
+  <Layout>
     <h1>
-      It has been <span className='highlight'>{_calcDate()} days</span> since my last bike accident 🙃
+      It has been <span className="highlight">{_calcDate()} days</span> since my
+      last bike accident 🙃
     </h1>
-    <Footer/>
-  </div>
-)
+    <Footer />
+  </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
